8a67ddba1395cbf8e40b618d6189c717954b5f0f
sergioalmazan <sergioalmazan22@gmail.com>
2020-12-14 13:51:27 -0800

    Added Questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,73 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "Title"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "Please provide the badges links that you want"
+    },
+    {
+        type: "input",
+        message: "Describe your project",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "How to install your project?",
+        name: "Installation"
+    },
+    {
+        type: "input",
+        message: "Instructions for use.",
+        name: "Usage"
+    },
+    {
+        type: "input",
+        message: "Are there other contributors?",
+        name: "Contributors"
+    },
+    {
+        type: "input",
+        message: "How do you test your project?",
+        name: "Test"
+    },
+    {
+        type: "input",
+        message: "Contact info for further questions",
+        name: "Questions"
+    },
+    {
+        type: "list",
+        message: "Select license",
+        name: "License",
+        choices: [
+            "MIT",
+            "GVL-GPL 3.0",
+            "APACHE 2.0",
+            "BSD 3",
+            "GPL",
+            "ISC",
+            "ZLIB",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        message: "Your Github username",
+        name: "Username"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "Email"
+    }
+    
 ];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
